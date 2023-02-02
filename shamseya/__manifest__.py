d269038606de75310b50e965443a295ffd1f3210
Helal1001 <63949422+Mohamed-S-Helal@users.noreply.github.com>
2023-02-02 23:10:38 +0200

    Update __manifest__.py

--- shamseya/__manifest__.py
+++ shamseya/__manifest__.py
@@ -23,9 +23,9 @@
 
     # always loaded
     'data': [
-#         'security/ir.model.access.csv',
-#         'views/views.xml',
-#         'data/data.xml'
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'data/data.xml'
         # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
