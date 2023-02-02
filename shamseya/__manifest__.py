69f818f97866331a2b4217f397a9c0ac578233e8
Mohamed Helal <m.helal@classera.com>
2023-02-02 21:58:58 +0200

    fix

--- shamseya/__manifest__.py
+++ shamseya/__manifest__.py
@@ -23,8 +23,8 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        # 'views/views.xml',
-        # 'data/data.xml'
+        'views/views.xml',
+        'data/data.xml'
         # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
